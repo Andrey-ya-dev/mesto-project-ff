3bcbd36bc2f9b0eedd2ab201fff8db6d69badc16
Andrey Ignashov <maylo7771@yandex.ru>
2024-08-20 14:03:53 +0300

    Добавлены методы для запросов к api

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -13,3 +13,78 @@ const Methods = {
   patch: "PATCH", // Update
   remove: "DELETE", // Delete
 };
+
+const handleResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+
+  return Promise.reject(`Ошибка : ${res.status}`);
+};
+
+export const getUser = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: Methods.get,
+    headers: config.headers,
+  }).then(handleResponse);
+};
+
+export const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: Methods.get,
+    headers: config.headers,
+  }).then(handleResponse);
+};
+
+export const updateProfile = (name, job) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: Methods.patch,
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      job,
+    }),
+  }).then(handleResponse);
+};
+
+export const updateAvatar = (link) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: Methods.patch,
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: link,
+    }),
+  }).then(handleResponse);
+};
+
+export const addNewCard = (name, link) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: Methods.post,
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      link,
+    }),
+  }).then(handleResponse);
+};
+
+export const removeCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
+    method: Methods.remove,
+    headers: config.headers,
+  }).then(handleResponse);
+};
+
+export const addLikeToCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+    method: Methods.put,
+    headers: config.headers,
+  }).then(handleResponse);
+};
+
+export const removeLikeToCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+    method: Methods.remove,
+    headers: config.headers,
+  }).then(handleResponse);
+};
